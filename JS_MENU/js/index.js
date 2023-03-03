40a82b5313793dde383fa784601918754236fbc9
Shovkat1987 <122811814+Shovkat1987@users.noreply.github.com>
2023-03-03 16:50:19 +0500

    Add files via upload

---
+++ JS_MENU/js/index.js
@@ -0,0 +1,27 @@
+
+
+const nav = document.querySelector('.navbar__menu');
+const menu = document.querySelector('.menu');
+
+const menuRemove = document.querySelector('.menu__bars');
+const menuRemove1 = document.querySelector('.menu__remove')
+const menuRemove2 = document.querySelector('.menu__remove__rem');
+
+menu.addEventListener('click', function () {
+    nav.classList.toggle('menu__active'); 
+})
+
+// menu.addEventListener('click', function () {
+//     menu.classList.toggle('menu__bars'); 
+// })
+
+// menu.addEventListener('click', function () {
+//     menuRemove1.classList.toggle('menu__remove__rem'); 
+// })
+
+
+ 
+
+
+
+
